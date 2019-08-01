5bfbdf35dc291177a46777ea0d77e111560d00f3
Shreyas Gaonkar <shreyas.gaonkar@yahoo.com>
2019-08-01 13:33:50 +0530

    add testimonial section

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,19 +1,74 @@
 $(document).ready(function() {
+  mentoringBubbleClick();
+});
 
+function mentoringBubbleClick() {
+  $('.face').on('click', function() {
 
+    var $this = $(this),
+      faceTop = $this.position().top,
+      vertMath = -1 * (faceTop - 250),
+      faceLeft = $this.position().left,
+      horizMath = 70 - faceLeft;
 
-  $(window).scroll(function() {
-    youtubeVidScroll();
+    // open bubble when clicked
+    $this.addClass('has-bubble-open')
+      .siblings().removeClass('has-bubble-open');
+    if ($(window).width() > 640) {
+      $this.parent().css('top', +vertMath + 'px');
+
+    } else {
+      $this.parent().css('left', +horizMath + 'px');
+      // $this.addClass('has-bubble-open')
+      //   .siblings().removeClass('has-bubble-open');
+    }
+
   });
 
-  function youtubeVidScroll() {
-    var wScroll = $(window).scrollTop();
 
-    // Only scroll when viewport in view
 
-    if (wScroll < 800) {
-      $('.video-strip').css('background-position', 'center -' + wScroll + 'px');
+  /*
+    1. Click a face
+    2. Get distance of the face from the top of the parent
+    3. Move container up 115px + count
+    4. add the is-open class to the face to pop the balloon
+  */
+
+}
+
+$(window).scroll(function() {
+  youtubeVidScroll();
+  startMentoring();
+});
+
+
+function startMentoring() {
+  var wScroll = $(window).scrollTop();
+
+  if (($('section.mentoring').offset().top - 500 < wScroll)) {
+
+    if (($(window).width() > 640)) {
+      $('.faces').addClass('launched');
+
+      if (!$('.face').hasClass("has-bubble-open")) {
+        setTimeout(function() {
+          $('.face:nth-child(3)').addClass("has-bubble-open");
+        }, 500);
+      }
+    } else {
+      // on mobile devices this should be fired asap
+      $('.faces').addClass('launched');
+      $('.face:nth-of-type(1)').addClass('has-bubble-open');
     }
   }
+}
 
-});
+function youtubeVidScroll() {
+  var wScroll = $(window).scrollTop();
+
+  // Only scroll when viewport in view
+
+  if (wScroll < 1200) {
+    $('.video-strip').css('background-position', 'center -' + (wScroll / 1.25) + 'px');
+  }
+}
